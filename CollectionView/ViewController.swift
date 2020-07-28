6a752af5b816fa3275c4485b227904d35169ce6f
Bassel Saleh <basselsaleh1234@gmail.com>
2020-07-28 12:55:57 +0200

    Initial Commit

---
+++ CollectionView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectionView
+//
+//  Created by Bassel Saleh on 28.07.20.
+//  Copyright © 2020 Bassel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
